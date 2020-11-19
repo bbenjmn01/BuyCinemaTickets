b0756ea021ac2e46d8e09fd37cc60e29bd656bbf
bbenjmn01 <bbenjmn01@gmail.com>
2020-11-19 19:55:01 +0700

    update seat

--- CinemaProject/src/Movie.java
+++ CinemaProject/src/Movie.java
@@ -9,24 +9,54 @@
  * @author User
  */
 public final class Movie {
-    
+
+    private Seats createSeat;
     private String[] movieName;
-    private int amount;
-    
-    public Movie(int a){
-        
-    }
-    
-    public Movie(String name){
-        insertMovieName(name);
-    }
-    
-    public void createMovieAmount(int a){
+    private int count = 0;
+    private int round = 0;
+
+    public Movie(int a) {
         movieName = new String[a];
-    } 
-    
-    public void insertMovieName(String name){
-        movieName[this.amount] = name;
     }
-            
+    
+    public Movie append(String name) {
+        if (count < movieName.length) {
+            movieName[count++] = name;
+            return this;
+        }
+        return null;
+    }
+
+    public void movieRound(int x, int y) {
+        ++round;
+        createSeat = new Seats(x, y);
+    }
+
+    public String[] getMovieName() {
+        return movieName;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb1 = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb1.append(movieName[i].toString()).append("\n");
+        }
+        
+        return sb1.toString() + getRound();
+    }
+    
+    public static void main(String[] args) {
+        Movie m = new Movie(3);
+        m.append("1. Benjamin");
+        m.append("2. Job-kun");
+        m.append("3. Malamild");
+        m.movieRound(10, 10);
+        System.out.println(m);
+    }
+    
 }
--- CinemaProject/src/Seats.java
+++ CinemaProject/src/Seats.java
@@ -10,25 +10,54 @@
  */
 public class Seats {
 
-    private boolean[][] seatStatus;
+    private String[][] seatStatus;
     private int count = 0;
+    private int a = 0;
     private int amount = 0;
 
     public Seats(int a, int b) {
-        seatStatus = new boolean[a][b];
+        seatStatus = new String[a][b];
+    }
+
+    public String show() {
+        for (int i = 0; i < seatStatus.length; i++) {
+            for (int j = 0; j < seatStatus[i].length; j++) {
+                    System.out.print(i+""+j+" : "+seatStatus[i][j] + "\t");
+                }
+            System.out.println();
+            }
+        return "";
     }
 
     public int buy(int a, int b) {
         ++count;
-        this.amount = count; 
-        seatStatus[a][b] = true;
+        this.amount = count;
+        seatStatus[a][b] = "Booked";
         this.count = 0;
         return this.amount;
     }
 
     public void resetSeats() {
         for (int i = 0; i < seatStatus.length; i++) {
-            seatStatus[i][i] = false;
+            for (int j = 0; j < seatStatus[i].length; j++) {
+                seatStatus[i][j] = null;
+            }
         }
     }
+
+    @Override
+    public String toString() {
+        return show();
+    }
+
+    public static void main(String[] args) {
+        Seats s = new Seats(10, 15);
+        s.buy(0, 0);
+        s.buy(9, 1);
+        s.buy(4, 2);
+        s.buy(6, 3);
+        s.buy(9, 4);
+        System.out.println(s);
+    }
+
 }
