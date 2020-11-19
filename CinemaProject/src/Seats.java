76cc3c5de9a7229f0e173dbe055929c84af072a9
bbenjmn01 <bbenjmn01@gmail.com>
2020-11-19 20:05:50 +0700

    edit bug buy

--- CinemaProject/src/Seats.java
+++ CinemaProject/src/Seats.java
@@ -11,30 +11,28 @@
 public class Seats {
 
     private String[][] seatStatus;
+    private final int row;
+    private final int col;
     private int count = 0;
     private int a = 0;
     private int amount = 0;
 
     public Seats(int a, int b) {
+        this.row = a;
+        this.col = b;
         seatStatus = new String[a][b];
     }
 
-    public String show() {
-        for (int i = 0; i < seatStatus.length; i++) {
-            for (int j = 0; j < seatStatus[i].length; j++) {
-                    System.out.print(i+""+j+" : "+seatStatus[i][j] + "\t");
-                }
-            System.out.println();
-            }
-        return "";
-    }
-
     public int buy(int a, int b) {
-        ++count;
-        this.amount = count;
-        seatStatus[a][b] = "Booked";
-        this.count = 0;
-        return this.amount;
+        if(a < this.row && a < this.col){
+            ++count;
+            this.amount = count;
+            seatStatus[a][b] = "Booked";
+            this.count = 0;
+            return this.amount;
+        }else{
+            return 0;
+        }
     }
 
     public void resetSeats() {
@@ -44,16 +42,22 @@ public class Seats {
             }
         }
     }
-
+    
     @Override
     public String toString() {
-        return show();
+        for (int i = 0; i < seatStatus.length; i++) {
+            for (int j = 0; j < seatStatus[i].length; j++) {
+                    System.out.print(i+""+j+" : "+seatStatus[i][j] + "\t");
+                }
+            System.out.println();
+            }
+        return "";
     }
 
     public static void main(String[] args) {
         Seats s = new Seats(10, 15);
         s.buy(0, 0);
-        s.buy(9, 1);
+        s.buy(10, 1);
         s.buy(4, 2);
         s.buy(6, 3);
         s.buy(9, 4);
